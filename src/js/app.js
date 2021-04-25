da2fab668d11b9516ed6d252c43ef2208d3612ab
lockness-Ko <videolk9373@gmail.com>
2021-04-25 18:03:31 +1000

    doing client side stuff

--- src/js/app.js
+++ src/js/app.js
@@ -29,42 +29,17 @@ App = {
       // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
 
-      App.listenForEvents();
+      // App.listenForEvents();
 
       return App.render();
     });
   },
 
-  // Listen for events emitted from the contract
-  listenForEvents: function() {
-    App.contracts.Election.deployed().then(function(instance) {
-      // Restart Chrome if you are unable to receive this event
-      // This is a known issue with Metamask
-      // https://github.com/MetaMask/metamask-extension/issues/2393
-      // instance.votedEvent({}, {
-      //   fromBlock: 0,
-      //   toBlock: 'latest'
-      // }).watch(function(error, event) {
-      //   console.log("event triggered", event)
-      //   // Reload when a new vote is recorded
-      //   App.render();
-      // });
-    });
-  },
-
   render: function() {
-    var electionInstance;
-    var loader = $("#loader");
-    var content = $("#content");
-
-    loader.show();
-    content.hide();
-
-    // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
+        // $("#accountAddress").html("Your Account: " + account);
       }
     });
 
@@ -73,50 +48,13 @@ App = {
       electionInstance = instance;
       return electionInstance.siteCount();
     }).then(function(siteCount) {
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
-
-      var candidatesSelect = $('#candidatesSelect');
-      candidatesSelect.empty();
-
-      for (var i = 1; i <= siteCount; i++) {
-        electionInstance.sites(i).then(function(site) {
-          var id = site[0];
-          var code = site[1];
-          var key = site[2];
-
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + code + "</td><td>" + key + "</td></tr>"
-          candidatesResults.append(candidateTemplate);
-
-          // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "' >" + code + "</ option>"
-          candidatesSelect.append(candidateOption);
-        });
-      }
-      return electionInstance.voters(App.account);
-    }).then(function(hasVoted) {
-      // Do not allow a user to vote
-      if(hasVoted) {
-        $('form').hide();
-      }
-      loader.hide();
-      content.show();
-    }).catch(function(error) {
-      console.warn(error);
-    });
-  },
-
-  castVote: function() {
-    var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function(instance) {
-      return instance.vote(candidateId, { from: App.account });
-    }).then(function(result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
-    }).catch(function(err) {
-      console.error(err);
+      // alert(siteCount);
+      //     var id = site[0];
+      //     var code = site[1];
+      //     var key = site[2];
+      electionInstance.sites(parseInt(window.location.href.split("=")[1])).then((x)=>{
+        document.write(x[1]);
+      });
     });
   }
 };
