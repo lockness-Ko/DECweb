491e93d45537c4e987785dde6230e79a13617dc0
lockness-Ko <videolk9373@gmail.com>
2021-04-25 17:39:08 +1000

    done with greeting the world, time to get real

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,8 @@ App = {
     // TODO: refactor conditional
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
+      App.web3Provider = ethereum;
+      web3 = new Web3(web3.ethereum);
     } else {
       // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
