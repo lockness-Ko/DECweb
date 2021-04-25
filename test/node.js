12a14f8ec4c406f0a8bdedffe17c14f76117914a
lockness-Ko <videolk9373@gmail.com>
2021-04-25 16:59:51 +1000

    hello world pt.3

---
+++ test/node.js
@@ -0,0 +1,11 @@
+var Node = artifacts.require('./Node.sol');
+
+contract("Node", (accounts)=>{
+    it("initializes with two sites", ()=>{
+        return Node.deployed().then((i)=>{
+            return i.siteCount();
+        }).then((count)=>{
+            assert.equal(count,2);
+        });
+    });
+})
